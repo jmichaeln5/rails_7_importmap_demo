dfb8b981c66a6a900a8fdddea7a28f19d065e380
Jon Norton <jmichaeln5@gmail.com>
2022-03-07 13:14:27 -0500

    Adding starter components for React + Vue with importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,20 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
+
+################ Vue
 pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js"
-# pin_all_from "vue_components"
 pin "vue_components/first_vue_component"
 pin "vue_components/second_vue_component"
+
+################ React
+pin "htm", to: "https://ga.jspm.io/npm:htm@3.1.0/dist/htm.module.js"
+pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@17.0.2/index.js"
+pin "object-assign", to: "https://ga.jspm.io/npm:object-assign@4.1.1/index.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.20.2/index.js"
+
+pin "react_components/htm_create_element"
+pin "react_components/first_react_component"
+pin "react_components/second_react_component"
+################
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/about'
 
-  # resources :posts
-
-  resources :posts do
-    # resources :react_posts, module: :react_posts
-    # resources :vue_posts, module: :vue_posts
-  end
+  resources :posts
 
   resources :react_posts, module: :posts
   resources :vue_posts, module: :posts
-
 end
